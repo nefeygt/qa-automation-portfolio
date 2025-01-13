e443f43b68a9ad78715a03644e8fe57e8bcf2a7a
nefeygt <n.efeyigit@outlook.com>
2025-01-13 14:23:04 +0300

    ci: Add GitHub Actions workflow for automated testing

--- utils/config_reader.py
+++ utils/config_reader.py
@@ -1,5 +1,10 @@
 import yaml
+import os
 
 def read_config():
-    with open('config/config.yaml', 'r') as file:
+    """Read configuration from YAML file"""
+    # Determine which config file to use
+    config_file = 'config/config.ci.yaml' if os.getenv('CI') else 'config/config.yaml'
+    
+    with open(config_file, 'r') as file:
         return yaml.safe_load(file)
